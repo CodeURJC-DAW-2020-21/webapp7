85a737475b7d1b5243538363efcc343189d8e159
DeborahIsraelVillanueva <deborahisraelvillanueva@gmail.com>
2021-04-01 21:29:26 +0200

    AJAX 50/50 half way through

--- src/main/java/com/webapp7/webapp7/service/PostService.java
+++ src/main/java/com/webapp7/webapp7/service/PostService.java
@@ -2,10 +2,14 @@ package com.webapp7.webapp7.service;
 
 import com.webapp7.webapp7.model.Post;
 import com.webapp7.webapp7.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import com.webapp7.webapp7.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.Optional;
 
 import java.util.List;
@@ -32,5 +36,15 @@ public class PostService {
 
     public Optional<Post> findById(long id) {
         return posts.findById(id);
+    }
+
+
+    public ArrayList<Post> findPost(Pageable pageable){
+        Page<Post> p=posts.findAll(pageable);
+        ArrayList<Post> aux=new ArrayList<Post>();
+        p.forEach(post->{
+                aux.add(post);
+        });
+        return aux;
     }
 }
