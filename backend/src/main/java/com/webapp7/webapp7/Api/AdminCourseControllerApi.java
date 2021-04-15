36aaacde3f94a39b5269afbc1fad63c985e9a15a
DeborahIsraelVillanueva <deborahisraelvillanueva@gmail.com>
2021-04-16 00:22:11 +0200

    Images bug fixed, deleting courses bug fixed,distorted images css fixed

--- backend/src/main/java/com/webapp7/webapp7/Api/AdminCourseControllerApi.java
+++ backend/src/main/java/com/webapp7/webapp7/Api/AdminCourseControllerApi.java
@@ -1,12 +1,19 @@
 package com.webapp7.webapp7.Api;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.webapp7.webapp7.Service.ImageService;
 import com.webapp7.webapp7.model.Course;
+import com.webapp7.webapp7.model.Post;
 import com.webapp7.webapp7.model.User;
+import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.util.Collection;
 import java.util.List;
 
@@ -16,10 +23,17 @@ import static org.springframework.web.servlet.support.ServletUriComponentsBuilde
 @RequestMapping("/api/admin/courses")
 public class AdminCourseControllerApi {
 
+    interface CourseBasic extends Course.Basic {}
+    private static final String COURSES_FOLDER = "courses";
+
     @Autowired
     private com.webapp7.webapp7.Service.CourseService courseService;
+
     @Autowired
     private com.webapp7.webapp7.Service.UserService userService;
+
+    @Autowired
+    private ImageService imgService;
 
     @JsonView(CourseBasic.class)
     @GetMapping("/")
@@ -39,6 +53,7 @@ public class AdminCourseControllerApi {
         return ResponseEntity.created(fromCurrentRequest().path("/").buildAndExpand(course.getId()).toUri()).body(course);
     }
 
+    @JsonView(CourseBasic.class)
     @PutMapping("/{id}")
     public ResponseEntity<Course> replacePost(@PathVariable long id,
                                               @RequestBody Course newCourse) {
@@ -54,19 +69,61 @@ public class AdminCourseControllerApi {
 
     @JsonView(CourseBasic.class)
     @DeleteMapping("/{id}")
-    public ResponseEntity<Course> deleteCourse(@PathVariable long id) {
-        User user = userService.findById(id).orElse(null);
-        Course course = courseService.findById(id).orElse(null);
-        if (course != null && user != null) {
-            user.setCourse(null);
-            userService.save(user);
+    public ResponseEntity<Course> deleteCourse(@PathVariable long id) throws IOException {
+        Course course = courseService.findById(id).orElseThrow(null);
+        List<User> users = course.getStudents();
+        User user;
+        for ( int i=1 ; i<users.size() ; i++ ) {
+            user = users.get(i);
+            if (user.getCourse()!=null && user.getCourse().getId()==id){
+                user.setCourse(null);
+                userService.save(user);
+            }
+        }
+        if (course != null ) {
             courseService.deleteById(id);
+
+            if(course.getImageFile() != null) {
+                this.imgService.deleteImage(COURSES_FOLDER,id);
+            }
+
             return ResponseEntity.ok(course);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
 
-    interface CourseBasic extends Course.Basic {
+    @JsonView(CourseBasic.class)
+    @PostMapping("/{id}/image")
+    public ResponseEntity<Object> uploadImage(@PathVariable long id, @RequestParam MultipartFile imageFile)
+            throws IOException {
+
+        Course course = courseService.findById(id).orElseThrow(null);
+
+        if (course != null) {
+
+            URI location = fromCurrentRequest().build().toUri();
+
+            if (!imageFile.isEmpty()) {
+                course.setImageFile(BlobProxy.generateProxy(imageFile.getInputStream(), imageFile.getSize()));
+                course.setImage(true);
+            }
+            courseService.save(course);
+
+            imgService.saveImage(COURSES_FOLDER, course.getId(), imageFile);
+
+            return ResponseEntity.created(location).build();
+
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @JsonView(CourseBasic.class)
+    @GetMapping("/{id}/image")
+    public ResponseEntity<Object> downloadImage(@PathVariable long id) throws MalformedURLException {
+        return this.imgService.createResponseFromImage(COURSES_FOLDER, id);
+
+
     }
 }
