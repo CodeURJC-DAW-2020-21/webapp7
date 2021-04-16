f68aa0cf8bdc09e61f37aa12d85a98829ec6970f
DeborahIsraelVillanueva <deborahisraelvillanueva@gmail.com>
2021-04-16 16:25:45 +0200

    trying to upload a file to postman

--- backend/src/main/java/com/webapp7/webapp7/Api/MaterialControllerApi.java
+++ backend/src/main/java/com/webapp7/webapp7/Api/MaterialControllerApi.java
@@ -5,8 +5,10 @@ import com.webapp7.webapp7.model.Material;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.Collection;
 import java.util.List;
 
@@ -15,6 +17,8 @@ import static org.springframework.web.servlet.support.ServletUriComponentsBuilde
 @RestController
 @RequestMapping("/api/materials")
 public class MaterialControllerApi {
+
+    interface MaterialBasic extends Material.Basic {}
 
     @Autowired
     private com.webapp7.webapp7.Service.MaterialService materialService;
@@ -33,16 +37,50 @@ public class MaterialControllerApi {
     @JsonView(MaterialBasic.class)
     @PostMapping("/")
     public ResponseEntity<Material> addMaterials(@RequestBody Material material) throws IOException {
-        /*if (material.getImageFile() != null) {
-            MultipartFile imageFile = (MultipartFile) material.getImageFile();
-            material.setImageFile(BlobProxy.generateProxy(imageFile.getInputStream(), imageFile.getSize()));
-        }
-
-         */
         materialService.save(material);
         return ResponseEntity.created(fromCurrentRequest().path("/").buildAndExpand(material.getId()).toUri()).body(material);
     }
-
-    interface MaterialBasic extends Material.Basic {
+    /*
+     @PostMapping("/user_instructor")
+    public String uploadfileInstructor(@RequestParam("material") MultipartFile multipartFile, HttpServletRequest request) throws IOException {
+        String username = request.getUserPrincipal().getName();
+        User user = userService.selectByEmail(username);
+        Course course = user.getCourse();
+        String fileName = multipartFile.getOriginalFilename();
+        Material material = new Material();
+        material.setCourse(course);
+        material.setName(fileName);
+        material.setContent(multipartFile.getBytes());
+        materialRepository.save(material);
+        return "redirect:/user_instructor";
     }
+     */
+
+    @JsonView(MaterialBasic.class)
+    @PostMapping("/{id}/file")
+    public ResponseEntity<Object> uploadFile(@PathVariable long id, @RequestParam MultipartFile multipartFile)
+            throws IOException {
+
+        Material material = materialService.findById(id).orElseThrow(null);
+
+        if (material != null) {
+
+            URI location = fromCurrentRequest().build().toUri();
+
+            if (!multipartFile.isEmpty()) {
+                String fileName = multipartFile.getOriginalFilename();
+                material.setCourse(material.getCourse());
+                material.setName(fileName);
+                material.setContent(multipartFile.getBytes());
+            }
+            materialService.save(material);
+
+            return ResponseEntity.created(location).build();
+
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 }
--- backend/src/main/java/com/webapp7/webapp7/Service/MaterialService.java
+++ backend/src/main/java/com/webapp7/webapp7/Service/MaterialService.java
@@ -1,11 +1,13 @@
 package com.webapp7.webapp7.Service;
 
 import com.webapp7.webapp7.model.Material;
+import com.webapp7.webapp7.model.Post;
 import com.webapp7.webapp7.repository.MaterialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MaterialService {
@@ -18,5 +20,9 @@ public class MaterialService {
 
     public void save(Material material) {
         materials.save(material);
+    }
+
+    public Optional<Material> findById(long id) {
+        return materials.findById(id);
     }
 }
