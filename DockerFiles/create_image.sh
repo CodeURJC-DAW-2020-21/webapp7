26956ca687b51c4328cc3f0aa4306b126031223b
DeborahIsraelVillanueva <deborahisraelvillanueva@gmail.com>
2021-04-18 23:48:24 +0200

    script update

--- DockerFiles/create_image.sh
+++ DockerFiles/create_image.sh
@@ -7,7 +7,9 @@ cd webapp7 || exit
 git clone https://github.com/CodeURJC-DAW-2020-21/webapp7
 
 cd webapp7/backend || exit
-
-cd docker || exit
-
-docker-compose up -d
+docker run --rm -e MYSQL_ROOT_PASSWORD=webapp7 -e MYSQL_DATABASE=kiddyshouse -p 3306:3306 -d mysql:8.0.22
+docker build -t webappy/webapp7 .
+docker push webapp7/webapp7:latest
+docker pull webapp7/webapp7
+docker run webapp7/webapp7
+docker-compose up -d --build
