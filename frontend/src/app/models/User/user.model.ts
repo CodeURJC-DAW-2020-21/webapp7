2c4826dc86edd71520d68a1ee78c83f2e5789345
DeborahIsraelVillanueva <deborahisraelvillanueva@gmail.com>
2021-04-25 01:27:00 +0200

    angular models

---
+++ frontend/src/app/models/User/user.model.ts
@@ -0,0 +1,13 @@
+import { Material } from '../Material/material.model';
+
+export interface User{
+  id:number;
+  email:string;
+  name:string;
+  password:string;
+  role:string;
+  //imageFile:string;
+  image:boolean;
+  //finishedMaterials:List<Material>
+  numberMaterials:number;
+}
