4170b8f480bc5a12d80c8cfcabb25c22b80bad61
DeborahIsraelVillanueva <deborahisraelvillanueva@gmail.com>
2021-04-25 21:18:21 +0200

    userService

--- frontend/src/app/components/user-admin/user-admin.component.ts
+++ frontend/src/app/components/user-admin/user-admin.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { User } from '../../models/User/user.model';
+import {Course} from '../../models/Course/course.model';
+import {UserService} from '../../services/user.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -6,11 +10,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../../../assets/css/style.css']
 })
 
-export class UserAdminComponent implements OnInit {
+export class UserAdminComponent {
 
-  constructor() { }
+  user: User;
+  course: Course;
+  data: FormData;
+  alert: boolean;
+  alertText: string;
 
-  ngOnInit(): void {
+  constructor(private router, private userService: UserService) {
+    this.user = {
+      email: '',
+      name: '',
+      password: '',
+      role: '',
+      image: false,
+      numberMaterials: 0
+    };
+  }
+
+  // tslint:disable-next-line:typedef
+  createUser( formulary: NgForm ) {
+
+    if ( formulary.invalid ) {
+      this.alertText = 'All fields must be Completed';
+      this.alert = true;
+
+      Object.values(formulary.controls).forEach( control => {
+        control.markAsTouched();
+      });
+    }
+
+    else {
+      this.alert = false;
+
+      this.data.append('jsondata', JSON.stringify(this.user));
+      this.data.append('password', this.user.password);
+
+      this.userService.createUser(this.data).subscribe(
+        response => {
+          this.router.navigate(['home']);
+        },
+        error => {
+          this.alertText = `El usuario no se pudo crear. \nContacte al servicio técnico.`;
+          this.alert = true;
+          this.router.navigateByUrl('/RefreshComponent', { skipLocationChange: true }).then(() => {
+            this.router.navigate(['user-admin']);
+          });
+        }
+      );
+    }
   }
 
 }
