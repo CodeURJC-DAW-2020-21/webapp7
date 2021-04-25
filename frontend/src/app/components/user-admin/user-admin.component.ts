ad9b3142e2a05ffdb49ca9dd5de0a94125020e6f
DeborahIsraelVillanueva <deborahisraelvillanueva@gmail.com>
2021-04-25 22:08:40 +0200

    postService

--- frontend/src/app/components/user-admin/user-admin.component.ts
+++ frontend/src/app/components/user-admin/user-admin.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
 import { User } from '../../models/User/user.model';
 import {Course} from '../../models/Course/course.model';
+import {Post} from '../../models/Post/post.model';
 import {UserService} from '../../services/user/user.service';
-import {NgForm} from '@angular/forms';
 import {CourseService} from '../../services/course/course.service';
+import {PostService} from '../../services/post/post.service';
+
 
 @Component({
   selector: 'app-admin',
@@ -15,14 +18,20 @@ export class UserAdminComponent {
 
   user: User;
   course: Course;
+  post: Post;
   userData: FormData;
   courseData: FormData;
+  postData: FormData;
   alert: boolean;
   alertText: string;
 
-  constructor(private router, private userService: UserService, private courseService: CourseService) {
+  constructor(private router,
+              private userService: UserService,
+              private courseService: CourseService,
+              private postService: PostService) {
     this.userData = new FormData();
     this.courseData = new FormData();
+    this.postData = new FormData();
     this.user = {
       email: '',
       name: '',
@@ -36,6 +45,11 @@ export class UserAdminComponent {
       ageStart: 0,
       ageEnd: 0,
       instructor: '',
+    };
+    this.post = {
+      title: '',
+      description: '',
+      image: false
     };
     this.alert = false;
   }
@@ -60,7 +74,7 @@ export class UserAdminComponent {
 
       this.userService.createUser(this.userData).subscribe(
         response => {
-          this.router.navigate(['home']);
+          this.router.navigate(['index']);
         },
         error => {
           this.alertText = `El usuario no se pudo crear. Contacte al servicio técnico.`;
@@ -91,16 +105,48 @@ export class UserAdminComponent {
 
       this.courseService.createCourse(this.courseData).subscribe(
         response => {
-          this.router.navigate(['home']);
+          this.router.navigate(['index']);
         },
         error => {
           this.alertText = 'El usuario no se pudo crear. Contacte al servicio técnico.';
           this.alert = true;
           this.router.navigateByUrl('/RefreshComponent', { skipLocationChange: true }).then(() => {
-            this.router.navigate(['/shelterform']);
+            this.router.navigate(['/user-admin']);
+          });
+        }
+      );
+    }
+  }
+
+  // tslint:disable-next-line:typedef
+  createPost( formulary: NgForm ) {
+    if ( formulary.invalid ) {
+      this.alertText = 'Todos los campos se deben rellenar';
+      this.alert = true;
+
+      Object.values(formulary.controls).forEach( control => {
+        control.markAsTouched();
+      });
+    }
+
+    else {
+      this.alert = false;
+
+      this.postData.append('jsondata', JSON.stringify(this.post));
+
+      this.postService.createPost(this.postData).subscribe(
+        response => {
+          this.router.navigate(['index']);
+        },
+        error => {
+          this.alertText = 'El post no se pudo crear. Contacte al servicio técnico.';
+          this.alert = true;
+          this.router.navigateByUrl('/RefreshComponent', { skipLocationChange: true }).then(() => {
+            this.router.navigate(['/user-admin']);
           });
         }
       );
     }
   }
 }
+
---
+++ frontend/src/app/services/post/post.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+const BASE_URL = environment.apiBase + '/courses/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  constructor(private http: HttpClient) { }
+
+  // tslint:disable-next-line:typedef
+  createPost(postData: FormData){
+    return this.http.post(BASE_URL, postData).pipe(
+      catchError(error => this.handleError(error))
+    );
+  }
+
+  // tslint:disable-next-line:typedef
+  private handleError(error: any) {
+    console.error(error);
+    return Observable.throw('Server error (' + error.status + '): ' + error.text());
+  }
+
+}
