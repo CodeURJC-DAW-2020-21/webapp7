6e47c4f4850efbe2ea5cc07860568fb5ac36085f
DeborahIsraelVillanueva <deborahisraelvillanueva@gmail.com>
2021-04-29 01:18:16 +0200

    components update

--- frontend/src/app/services/user/user.service.ts
+++ frontend/src/app/services/user/user.service.ts
@@ -3,6 +3,8 @@ import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import {User} from '../../models/User/user.model';
+import {Course} from '../../models/Course/course.model';
 
 const BASE_URL = environment.apiBase + '/users/';
 
@@ -14,15 +16,33 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   // tslint:disable-next-line:typedef
-  createUser( userData: FormData ) {
-    return this.http.post(BASE_URL, userData).pipe(
+ addUser( user: User ) {
+    return this.http.post(BASE_URL, user).pipe(
       catchError(error => this.handleError(error))
     );
   }
+/*
+  addUserToACourse(user: User,course:Course){
+    course.
+  }
+
+ */
   // tslint:disable-next-line:typedef
   public getUser(id: number) {
     const url = environment.apiBase + '/users/' + id;
     return this.http.get(url);
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get(BASE_URL).pipe(
+      catchError(error => this.handleError(error))
+    ) as Observable<User[]>;
+  }
+
+  removeUser(user: User) {
+    return this.http.delete(BASE_URL + user.id).pipe(
+      catchError(error => this.handleError(error))
+    );
   }
 
   // tslint:disable-next-line:typedef
