e4454d5b9289d901fdd4994d0da779b046fe834a
mrjesuland <jesuxever@hotmail.es>
2021-05-02 22:02:50 +0200

    show posts in Blog and Index

--- frontend/src/app/components/blog/blog.component.ts
+++ frontend/src/app/components/blog/blog.component.ts
@@ -1,6 +1,8 @@
 
 import { Component, OnInit } from '@angular/core';
 import {Post} from '../../models/Post/post.model';
+import {PostService} from '../../services/post/post.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 
 @Component({
@@ -13,9 +15,23 @@ export class BlogComponent implements OnInit {
   post: Post;
   posts: Post[];
   restOfPosts: Post;
-  
-  constructor() { }
-  
+
+  constructor(private router: Router,
+              public postService: PostService,
+              activateRoute: ActivatedRoute) {
+
+    const id = activateRoute.snapshot.params['id'];
+
+    postService.getPosts().subscribe(
+      posts => this.posts = posts,
+      error => console.error(error)
+    );
+
+
+
+
+  }
+
   ngOnInit(): void {
   }
 
--- frontend/src/app/components/index/index.component.ts
+++ frontend/src/app/components/index/index.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Post} from '../../models/Post/post.model';
+import {ActivatedRoute, Router} from '@angular/router';
+import {PostService} from '../../services/post/post.service';
 
 @Component({
   selector: 'app-index',
@@ -7,7 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  post: Post;
+  posts: Post[];
+
+  constructor(private router: Router,
+              public postService: PostService,
+              activateRoute: ActivatedRoute) {
+
+    const id = activateRoute.snapshot.params['id'];
+
+    postService.getPosts().subscribe(
+      posts => this.posts = posts,
+      error => console.error(error)
+    );
+  }
 
   ngOnInit(): void {
   }
